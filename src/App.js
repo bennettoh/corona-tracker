a46b8e650e040ad001ce26180d2c9c4e64c2c6f1
bennettoh <bennettoh.arch@gmail.com>
2020-08-30 13:21:30 -0700

    initialize modular components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import { ReactComponent } from '*.svg';
 
-class App extends ReactComponent {
+// import Cards from './components/Cards/Cards';
+// import Chart from './components/Chart/Chart';
+// import CountryPicker from './components/CountryPicker/CountryPicker';
+
+import {Cards, Chart, CountryPicker} from './components';
+import styles from './App.module.css';
+import {fetchData} from './api';
+
+class App extends React.Component {
+    state = {
+        data: {},
+    }
+
+    async componentDidMount(){
+        const fetchedData = await fetchData();
+
+        this.setState = {data: fetchedData};
+    }
+
     render(){
+        const {data} = this.state;
         return (
-            <div>
-                <h1>App</h1>
+            <div className={styles.container}>
+                <Cards data = {data}/>
+                <Chart />
+                <CountryPicker />
             </div>
         )
     }
---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cards = (props) => {
+    console.log(props);
+    
+    return(
+        <h1>Cards</h1>
+    )
+}
+
+export default Cards;
