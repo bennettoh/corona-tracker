276f2438930cc97cb989d3ab3c569c39014554f8
bennettoh <bennettoh.arch@gmail.com>
2020-09-04 17:24:19 -0700

    Updates

--- precache-manifest.0a497fdb3fe482b1d28509a9786a35a0.js
+++ precache-manifest.0a497fdb3fe482b1d28509a9786a35a0.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "28c2b53f0d317ae5374147cd42dd96d8",
+    "revision": "59d006199e749b45d69f3265a7f4a3d1",
     "url": "/corona-tracker/index.html"
   },
   {
-    "revision": "cfa76fed3ac9ecefc6b7",
+    "revision": "f2d87f2b1c3847df0052",
     "url": "/corona-tracker/static/css/main.4b584eb9.chunk.css"
   },
   {
-    "revision": "121c1d541b950be004c7",
-    "url": "/corona-tracker/static/js/2.8dc8aca1.chunk.js"
+    "revision": "cd104568c66ee0b54dc4",
+    "url": "/corona-tracker/static/js/2.62e3c4e2.chunk.js"
   },
   {
     "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
-    "url": "/corona-tracker/static/js/2.8dc8aca1.chunk.js.LICENSE.txt"
+    "url": "/corona-tracker/static/js/2.62e3c4e2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "cfa76fed3ac9ecefc6b7",
-    "url": "/corona-tracker/static/js/main.dcdc1dec.chunk.js"
+    "revision": "f2d87f2b1c3847df0052",
+    "url": "/corona-tracker/static/js/main.11eb5843.chunk.js"
   },
   {
     "revision": "9c4d7b1cf1de7ae99f79",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/corona-tracker/precache-manifest.2c15bbcda79360948a845448f05a0888.js"
+  "/corona-tracker/precache-manifest.0a497fdb3fe482b1d28509a9786a35a0.js"
 );
 
 self.addEventListener('message', (event) => {
